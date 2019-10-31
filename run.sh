2873c51217cda6dc0da43cbe5620b5f2f8f12b1d
Matt Wala <wala1@illinois.edu>
2019-10-31 04:04:28 -0500

    Update run.sh

--- run.sh
+++ run.sh
@@ -1,43 +1,17 @@
 #!/bin/bash -e
 
-# Find platform
-if [ "$(uname)" = Darwin ] && [ "$(uname -m)" = x86_64 ]
-then
-    MINICONDA_INSTALL_SH=Miniconda3-4.7.10-MacOSX-x86_64.sh
-    SPEC_FILE=env/osx-64-spec.txt
-elif [ "$(uname)" = Linux ] && [ "$(uname -m)" = x86_64 ]
-then
-    MINICONDA_INSTALL_SH=Miniconda3-4.7.10-Linux-x86_64.sh
-    SPEC_FILE=env/linux-64-spec.txt
-else
-    echo Unsupported platform.
-    exit 1
-fi
+# This script re-runs most of the experiments, except the long-running
+# plane-bvp experiment.
 
-MINICONDA_INSTALL_DIR=.miniconda3
-
-if [ -d $MINICONDA_INSTALL_DIR ] && [ -d src ]
-then
-    # Re-activate environment
-    source "$MINICONDA_INSTALL_DIR/bin/activate" inteq
-else
-    # Install miniconda
-    curl -O https://repo.continuum.io/miniconda/$MINICONDA_INSTALL_SH
-    bash $MINICONDA_INSTALL_SH -b -p "$MINICONDA_INSTALL_DIR"
-
-    # Install software
-    PATH="$MINICONDA_INSTALL_DIR/bin:$PATH" conda create --yes --name inteq --file ${SPEC_FILE}
-    source "$MINICONDA_INSTALL_DIR/bin/activate" inteq
-    pip install -r env/requirements.txt
-fi
+# Install/activate environment
+source install.sh
 
 # Tell PyOpenCL to use POCL
 export PYOPENCL_TEST=portable
 
 # Run a simple test
-py.test --disable-warnings utils.py
+py.test --disable-warnings utils.py inteq_tests.py
 
 # Run code
-OMP_NUM_THREADS=1 ./generate-data.py -x wall-time
-nice -n 1 ./generate-data.py --all --except wall-time
-./generate-figures-and-tables.py --all
+nice ./generate-data.py --all --except '*-calibration-params' --except plane-bvp
+make -f makefile.summary
